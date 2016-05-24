7e5f6a223fefe9713c127341d8f1928c49e26d70
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-05-24 14:19:56 -0400

    Updated Person Class to include Name Property and two constructors

--- Comp123-w2016-Lesson3/Person.cs
+++ Comp123-w2016-Lesson3/Person.cs
@@ -6,9 +6,57 @@ using System.Threading.Tasks;
 
 namespace Comp123_s2016_Lesson3
 {
+    /**
+    * this class defines the generic Person
+    *
+    * @class person
+    * @field _name {string}
+    */
     public class Person
     {
-        // Private Instance Variables
+        // Private Instance Variables ++++++++++++++++++++++++++++++++++++++++++++++++
         private string _name;
+
+        // Public properties +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        
+        /**
+        * This is a property for our _name field
+        *
+        * @property {string} name
+        */
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                this._name = value;
+            }
+
+        }
+
+        /**
+        * This is the default empty constructor for the Person Class
+        *
+        * @constructor Person
+        */
+        public Person()
+        {
+            this._name = "Unknown name!";
+        }
+        /**
+        * This is a constructor that takes name as a parameter and passes it 
+        * to the _name private instance variable
+        *
+        * @constructor Person
+        * @param {string} name
+        */
+        public Person(string name)
+        {
+            this._name = name;
+        }
     }
 }
--- Comp123-w2016-Lesson3/Program.cs
+++ Comp123-w2016-Lesson3/Program.cs
@@ -6,11 +6,35 @@ using System.Threading.Tasks;
 
 namespace Comp123_s2016_Lesson3
 {
+    /**
+    * this class is the driver class for our Program
+    *
+    * @class Program
+    */
     public class Program
     {
+        /**
+        * the main method for our driver class Program
+        *
+        * @method main
+        * @param {string[]} args
+        */
         public static void Main(string[] args)
         {
-            
+            // can change lowercase 'person' to anything else like your name or 'tom'
+            Person person;
+
+            // create a new instance of the Person class
+            // or Person person = new Person();
+            person = new Person("Tom");
+
+            Console.WriteLine(person.Name);
+
+            // set a value in the Name property of the Person Class
+            person.Name = "Tom";
+
+            // get the name value and print to console.
+            Console.WriteLine(person.Name);
         }
     }
 }
